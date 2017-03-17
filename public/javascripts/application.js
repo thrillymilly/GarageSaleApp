c2671dfff95072b2de4f42af03969a242f9c732a
William Miller <will.oliver.miller@gmail.com>
2017-03-17 13:29:35 +1100

    mvp v.0.1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,6 @@ console.log("JS WORKING");
 var geocoder;
   var map;
       function initMap() {
-        console.log('sdfsdfsd');
         geocoder = new google.maps.Geocoder();
         var latlng = new google.maps.LatLng(-37.814236, 144.955527);
         var mapOptions = {
@@ -22,21 +21,13 @@ var geocoder;
         var houseNumber = document.getElementsByClassName('house-number')[0].innerHTML;
         var postcode = document.getElementsByClassName('postcode')[0].innerHTML;
         var streetName = document.getElementsByClassName('street-name')[0].innerHTML;
-        //
-
-
         var totalAddress = houseNumber + ' ' + streetName + ' ' + postcode;
-        console.log(totalAddress);
         geocoder.geocode( { address: totalAddress }, function(results, status) {
-          // if (status == google.maps.GeocoderStatus.OK) {
-            // map.setCenter(results[0].geometry.location);
 
             var marker = new google.maps.Marker({
                 map: map,
-                position: results[0].geometry.location //{ lat: -37.814236, lng: 144.955527}
+                position: results[0].geometry.location
             });
-          // // } else {
-            // alert('Geocode was not successful for the following reason: ' + status);
+
           });
-        // };
       }
